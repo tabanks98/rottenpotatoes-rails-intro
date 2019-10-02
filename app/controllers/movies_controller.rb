8ca55a4d21ce3111bb98664c5f72a985adc68ee4
Tate Banks <tabanks98@gmail.com>
2019-10-02 00:12:18 -0500

    bull fucking shit. Fuck this assignment and everyone who assigns it

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  helper_method :hilight
+  helper_method :chosen_rating?
 
   def show
     id = params[:id] # retrieve movie ID from URI route
@@ -7,22 +9,23 @@ class MoviesController < ApplicationController
   end
 
   def index
-		if params.key?(:sort_by)
-			session[:sort_by] = params[:sort_by]
-		elsif session.key?(:sort_by)
-			params[:sort_by] = session[:sort_by]
-			redirect_to movies_path(params) and return
-		end
-		@hilite = sort_by = session[:sort_by]
-		@all_ratings = Movie.all_ratings
-		if params.key?(:ratings)
-			session[:ratings] = params[:ratings]
-		elsif session.key?(:ratings)
-			params[:ratings] = session[:ratings]
-			redirect_to movies_path(params) and return
-		end
-		@checked_ratings = (session[:ratings].keys if session.key?(:ratings)) || @all_ratings
-    @movies = Movie.order(sort_by).where(rating: @checked_ratings)
+      @all_ratings = ['G','PG','PG-13','R']
+      session[:ratings] = params[:ratings] unless params[:ratings] == nil
+      session[:order] = params[:order] unless params[:order] == nil
+      if ((params[:order] == nil) && !(session[:order] == nil)) || ((params[:ratings] == nil) && !(session[:ratings] == nil))
+        redirect_to movies_path("ratings" => session[:ratings], "order" => session[:order])
+      elsif !(params[:ratings] == nil) || !(params[:order] == nil)
+        if !(params[:order] == nil)
+          return @movies = Movie.all.order(session[:order])
+        else
+          array_ratings = params[:ratings].keys
+          return @movies = Movie.where(rating: array_ratings).order(session[:order])
+        end
+      elsif !(session[:ratings] == nil) || !(session[:order] == nil)
+        redirect_to movies_path("ratings" => session[:ratings], "order" => session[:order])
+      else
+        return @movies = Movie.all
+      end
   end
 
   def new
@@ -53,4 +56,18 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def hilightColumn(column)
+      if(session[:order].to_s == column)
+        return 'hilite'
+      else
+        return nil
+      end
+    end
+
+  def chosen_rating?(rating)
+      chosen_ratings = session[:ratings]
+      return true if chosen_ratings.nil?
+      chosen_ratings.include? rating
+  end
+    
 end
