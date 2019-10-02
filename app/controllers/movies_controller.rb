64cbb704e772509d1f1dd3193b2f4af45ff8570e
Tate Banks <tabanks98@gmail.com>
2019-10-01 20:52:48 -0500

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,9 +12,8 @@ class MoviesController < ApplicationController
 
   def index
       @all_ratings = ['G','PG','PG-13','R']
-      session[:ratings] = params[:ratings] unless params[:ratings].nil?
+      session[:ratings] = params[:ratings] unless params[:ratings] == nil
       session[:order] = params[:order] unless params[:order].nil?
-
       if (params[:ratings].nil? && !session[:ratings].nil?) || (params[:order].nil? && !session[:order].nil?)
         redirect_to movies_path("ratings" => session[:ratings], "order" => session[:order])
       elsif !params[:ratings].nil? || !params[:order].nil?
@@ -29,7 +28,7 @@ class MoviesController < ApplicationController
       else
         return @movies = Movie.all
       end
-    end
+  end
 
   def new
     # default: render 'new' template
