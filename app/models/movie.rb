002eb1f7f176f3a6ad690f296f9b2566caa05acb
Tate Banks <tabanks98@gmail.com>
2019-10-02 00:24:01 -0500

    blajf

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,3 @@
 class Movie < ActiveRecord::Base
-  
-  @@all_ratings = ['G','PG','PG-13','R']
-
-  	def self.all_ratings
-  		@@all_ratings
-  	end
+  attr_accessible :title, :rating, :description, :release_date
 end
